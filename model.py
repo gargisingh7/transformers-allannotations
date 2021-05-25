de6ab23458d9241af6a7c3c49143dfff6ad682b0
garsin01 <garsinhere@gmail.com>
2021-05-25 21:22:14 +0530

    xlnet files

--- model.py
+++ model.py
@@ -1,41 +1,55 @@
 import torch.nn as nn
-from transformers import BertPreTrainedModel, BertModel
+from transformers import XLNetPreTrainedModel, XLNetModel
+from transformers.modeling_utils import SequenceSummary
 
-
-class BertForMultiLabelClassification(BertPreTrainedModel):
+class BertForMultiLabelClassification(XLNetPreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
         self.num_labels = config.num_labels
 
-        self.bert = BertModel(config)
-        self.dropout = nn.Dropout(config.hidden_dropout_prob)
-        self.classifier = nn.Linear(config.hidden_size, self.config.num_labels)
+        self.transformer = XLNetModel(config)
+        self.sequence_summary = SequenceSummary(config)
+        self.logits_proj = nn.Linear(config.d_model, config.num_labels)
         self.loss_fct = nn.BCEWithLogitsLoss()
 
         self.init_weights()
 
     def forward(
-            self,
-            input_ids=None,
-            attention_mask=None,
-            token_type_ids=None,
-            position_ids=None,
-            head_mask=None,
-            inputs_embeds=None,
-            labels=None,
+        self,
+        input_ids=None,
+        attention_mask=None,
+        mems=None,
+        perm_mask=None,
+        target_mapping=None,
+        token_type_ids=None,
+        input_mask=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        use_mems=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
     ):
-        outputs = self.bert(
+        outputs = self.transformer(
             input_ids,
             attention_mask=attention_mask,
+            mems=mems,
+            perm_mask=perm_mask,
+            target_mapping=target_mapping,
             token_type_ids=token_type_ids,
-            position_ids=position_ids,
+            input_mask=input_mask,
             head_mask=head_mask,
             inputs_embeds=inputs_embeds,
+            use_mems=use_mems,
+            output_attentions=output_attentions,
+            output_hidden_states=output_hidden_states,
+            return_dict=return_dict,
         )
-        pooled_output = outputs[1]
+        output = outputs[0]
 
-        pooled_output = self.dropout(pooled_output)
-        logits = self.classifier(pooled_output)
+        output = self.sequence_summary(output)
+        logits = self.logits_proj(output)
 
         outputs = (logits,) + outputs[2:]  # add hidden states and attention if they are here
 
